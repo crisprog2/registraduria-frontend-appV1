bd9bf0c83e26ea28c7e7fb45ff277c309030fc9c
christian chacon <christian_kazuma@hotmail.com>
2022-02-02 15:40:15 -0500

    Se termina de implementar la funcionalidad de Eliminar al componente Ciudad

--- src/app/ciudad/eliminar-ciudad.component.ts
+++ src/app/ciudad/eliminar-ciudad.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Ciudad } from './ciudad';
+import { CiudadService } from './ciudad.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-eliminar-ciudad',
@@ -7,9 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EliminarCiudadComponent implements OnInit {
 
-  constructor() { }
+  titulo: string="Eliminar Ciudad";
+  ciudad: Ciudad= new Ciudad();
+
+  constructor(private ciudadService: CiudadService, private router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    this.consultaCiudad();
+
+  }
+
+  consultaCiudad(): void {
+    this.activatedRoute.params.subscribe(params => {
+      let id = params['id']
+      if(id){
+        this.ciudadService.getCiudad(id).subscribe(
+          ciudad=>this.ciudad=ciudad
+        )
+      }
+    })
+  }
+
+  public deleteCiudad(): void {
+
+    Swal.fire({
+      title: '¿Esta seguro de que quiere eliminar la Ciudad?',
+      text: "¡Esta acción no se puede cambiar!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.ciudadService.deleteCiudad(this.ciudad.codCiudad).subscribe( ciudad => {
+          this.router.navigate(['/ciudades'])
+        })
+        Swal.fire(
+          'Borrado!',
+          'La Ciudad ha sido Borrada.',
+          'success'
+        )
+      }
+    })
   }
 
 }

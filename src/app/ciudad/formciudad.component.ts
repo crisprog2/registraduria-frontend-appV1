488dad7b73b586d830a70e266e8e7d25e049579f
christian chacon <christian_kazuma@hotmail.com>
2022-02-02 13:35:50 -0500

    Se observa un error de logica en el form de departamento se tiene que crear un form distinto

--- src/app/ciudad/formciudad.component.ts
+++ src/app/ciudad/formciudad.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Departamento } from '../departamento/departamento';
+import { DepartamentoService } from '../departamento/departamento.service';
+import { Ciudad } from './ciudad';
+import { CiudadService } from './ciudad.service';
+import swal from 'sweetalert2'
 
 @Component({
   selector: 'app-formciudad',
@@ -7,9 +13,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormciudadComponent implements OnInit {
 
-  constructor() { }
+  departamentos: Departamento[] | undefined;
+  public ciudad: Ciudad = new Ciudad();
+  public titulo: string="Crear/Actualizar Ciudad";
+
+  constructor(private departamentoService: DepartamentoService, private ciudadService: CiudadService, private router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.departamentoService.getDepartamentos().subscribe(
+      departamentos => this.departamentos = departamentos
+    );
+  }
+
+  public create(): void{
+    this.ciudadService.createCiudad(this.ciudad).subscribe(
+      ciudad => {
+        this.router.navigate(['/ciudades'])
+        swal.fire('Nueva Ciudad', `Ciudad ${ciudad.ciudad} creada con exito`, 'success')
+      }
+    )
+  }
+
+  public update(): void{
+    
   }
 
 }
